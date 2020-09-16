da66d54f007b8414a2df36ccbc491e5615ef5ab9
SuwannapongIntawongMost <37680356+SuwannapongIntawongMost@users.noreply.github.com>
2020-09-16 13:42:00 +0700

    test js

---
+++ example.js
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+// let name ="John"
+// name = "Joke"
+
+let test = "Hello"
+name =  `${test} B`
+
+let person = {
+    name : 'Most' ,
+    Age : 15 ,
+    gender : 'male'
+}
+
+console.log(person.name)
+
+let arr = ['apple','banana','kiwi']
+console.log("arr : ", arr[1])
+
