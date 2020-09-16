4268078ff668ca66760202157a0d2470ecd4be32
SuwannapongIntawongMost <37680356+SuwannapongIntawongMost@users.noreply.github.com>
2020-09-16 13:47:29 +0700

    EX01

--- example.js
+++ example.js
@@ -1,19 +1,29 @@
-console.log("Hello World")
+// console.log("Hello World")
 
 // let name ="John"
 // name = "Joke"
 
-let test = "Hello"
-name =  `${test} B`
+// let test = "Hello"
+// name =  `${test} B`
 
-let person = {
-    name : 'Most' ,
-    Age : 15 ,
-    gender : 'male'
+// let person = {
+//     name : 'Most' ,
+//     Age : 15 ,
+//     gender : 'male'
+// }
+
+// console.log(person.name)
+
+// let arr = ['apple','banana','kiwi']
+// console.log("arr : ", arr[1])
+
+let data = {
+    name : 'eiei' ,
+    lastname : 'hihi',
+    Age : 21 ,
+    Nickname : 'haha',
+    Hobbies : ['playgame', 'sports'],
+    Phone_number :  '099-999-9999'
 }
 
-console.log(person.name)
-
-let arr = ['apple','banana','kiwi']
-console.log("arr : ", arr[1])
-
+console.log(data)
